7bd8147807f8361fa45ec023260814e56e2e314e
Christian Sonne <cers@geeksbynature.dk>
2013-01-25 01:30:03 -0800

    Now uses audio generated via audio API

--- helicopter.js
+++ helicopter.js
@@ -33,15 +33,6 @@ function Helicopter(e, settings) {
     this.fireball[i] = img;
   }
 
-  this.audio = new Audio();
-  this.audio.src="data/helicopter.ogg";
-  this.audio.loop = true;
-  this.audio.controls = false;
-
-  this.rev = new Audio();
-  this.rev.src="data/rev.ogg";
-  this.rev.loop = false;
-  this.rev.controls = false;
 
   this.highscore = localStorage.getItem("highscore") || 0;
 
@@ -58,12 +49,9 @@ function Helicopter(e, settings) {
     if (!self.runId)
       self.startGame();
     self.mouseDown = true;
-    if (self.settings.sound && self.rev.paused && self.runId)
-      self.rev.play();
   }, false);
   e.addEventListener(stopEvent, function H_mouseUp() {
     self.mouseDown = false;
-    self.rev.pause();
   }, false);
 
   this.init();
@@ -94,8 +82,6 @@ Helicopter.prototype = {
     sound: false
   },
   mapData: [],
-  audio: null,
-  rev: null,
   highscore: 0,
   scorecanvas: null,
   scorectx: null,
@@ -103,10 +89,75 @@ Helicopter.prototype = {
   smoke: [],
   fireball: [],
   fireballCnt: 0,
-  course: function H_course(x) {
-    var x = x + this.offset;
-    var tmp = Math.sin(x/this.width)*this.height/4;
-    return [this.height/2-tmp, this.height-this.height/4-tmp];
+  audio: null,
+  audioWritePosition: 0,
+  audioTailPosition: 0,
+  audioTail: null,
+  audioSampleRate: 24000,
+  audioPreBufferSize: 2165,
+  audioData: [-0.09094767272472382,  -0.2526524104177952,  -0.43981466442346573, -0.5683957412838936,  -0.6685325503349304,  -0.7610641233623028,
+              -0.8337558433413506,   -0.9083685278892517,  -0.9457360580563545,  -0.9965807944536209,  -1.023292914032936,   -0.9815550595521927,
+              -1.03635773062706,     -1.070520356297493,   -1.0548019409179688,  -1.0660066455602646,  -1.0139277577400208,  -1.0234511643648148,
+              -1.0528356581926346,   -1.1173677071928978,  -1.2371886894106865,  -1.304195076227188,   -1.4315441250801086,  -1.5686699375510216,
+              -1.7009016126394272,   -1.8144557252526283,  -1.8877371400594711,  -2.0182478427886963,  -2.1724480390548706,  -2.313513904809952,
+              -2.407410442829132,    -2.469414174556732,   -2.5162379443645477,  -2.5239667296409607,  -2.5158382952213287,  -2.4929867684841156,
+              -2.424892634153366,    -2.3242785036563873,  -2.1730680763721466,  -1.925196647644043,   -1.6867701709270477,  -1.5479131042957306,
+              -1.397036835551262,    -1.2590369582176208,  -1.1687033995985985,  -0.9599556773900986,  -0.7865398563444614,  -0.7191794738173485,
+              -0.5652515217661858,   -0.40363356471061707, -0.2950473502278328,  -0.15537556260824203, -0.04554618266411126,  0.005254739080555737,
+               0.09829435497522354,   0.13388743624091148,  0.1481253281235695,   0.198993980884552,    0.165505763143301,    0.1737920567393303,
+               0.23418329190462828,   0.20897026173770428,  0.26167692616581917,  0.45426436699926853,  0.5520237050950527,   0.6647061556577682,
+               0.8007090166211128,    0.8031865954399109,   0.8712762035429478,   0.916176326572895,    0.8830716647207737,   0.938393622636795,
+               0.9455873072147369,    0.9044785425066948,   0.9467797726392746,   0.9554203972220421,   0.9054944291710854,   0.9261629730463028,
+               0.920693501830101,     0.8621784299612045,   0.8885195106267929,   0.8676645532250404,   0.8237006887793541,   0.8529844880104065,
+               0.8028267323970795,    0.7733338885009289,   0.786685086786747,    0.7433289662003517,   0.7523776218295097,   0.7665916532278061,
+               0.7151978462934494,    0.7276155799627304,   0.737028568983078,    0.6831571832299232,   0.7123416289687157,   0.7181349769234657,
+               0.6593722477555275,    0.7010294124484062,   0.7306433469057083,   0.6620519980788231,   0.6828459352254868,   0.6920991092920303,
+               0.652933157980442,     0.6998000666499138,   0.6612315773963928,   0.6313015334308147,   0.6524486280977726,   0.5917346477508545,
+               0.6127200834453106,    0.6734062358736992,   0.6164751201868057,   0.611715205013752,    0.6467445194721222,   0.5786931328475475,
+               0.585954375565052,     0.6068623065948486,   0.557165052741766,    0.5865903943777084,   0.5936172790825367,   0.5323352292180061,
+               0.5593478679656982,    0.570390522480011,    0.5139668472111225,   0.5469214729964733,   0.528421550989151,    0.48812907189130783,
+               0.5256107077002525,    0.4865284636616707,   0.46506134793162346,  0.49725528806447983,  0.4604901373386383,   0.4553905874490738,
+               0.4918215796351433,    0.4472533240914345,   0.451140683144331,    0.4121099039912224,   0.19445814192295074,  0.0701064895838499,
+              -0.038083100225776434, -0.18951355945318937, -0.21401728503406048, -0.27909770607948303, -0.34035226330161095, -0.30652941204607487,
+              -0.3428588155657053,   -0.35870625637471676, -0.3065003827214241,  -0.353024136275053,   -0.35746296867728233, -0.3164553176611662,
+              -0.3850987181067467,   -0.3479635063558817,  -0.30901065096259117, -0.3495414834469557,  -0.29775258153676987, -0.2996205724775791,
+              -0.3083164896816015,   -0.2521904557943344,  -0.27333028614521027, -0.28864307329058647, -0.2239586552605033,  -0.18037700559943914,
+              -0.08798559196293354,   0.010952996090054512, 0.004114396870136261],
+  handleAudio: function H_handleAudio() {
+    var written;
+    // Check if some data was not written in previous attempts.
+    if(this.audioTail) {
+      written = this.audio.mozWriteAudio(this.audioTail.subarray(this.audioTailPosition));
+      this.audioWritePosition += written;
+      this.audioTailPosition += written;
+      if(this.audioTailPosition < this.audioTail.length) {
+        // Not all the data was written, saving the tail...
+        return; // ... and exit the function.
+      }
+      this.audioTail = null;
+    }
+    // Check if we need add some data to the audio output.
+    var currentPosition = this.audio.mozCurrentSampleOffset();
+    var available = currentPosition + this.audioPreBufferSize - this.audioWritePosition;
+    if(available > 0) {
+      // Request some sound data from the callback function.
+      var soundData = new Float32Array(2165);
+      for (var o=0; o<this.audioData.length; o++) {
+        soundData[o] = this.audioData[o];
+      }
+      for (var o=165; o<soundData.length; o++) {
+        soundData[o] = 0;
+      }
+
+      // Writting the data.
+      written = this.audio.mozWriteAudio(soundData);
+      if(written < soundData.length) {
+        // Not all the data was written, saving the tail.
+        this.audioTail = soundData;
+        this.audioTailPosition = written;
+      }
+      this.audioWritePosition += written;
+    }
   },
   init: function H_init() {
     this.playerX = this.width/5;
@@ -124,6 +175,11 @@ Helicopter.prototype = {
     this.ctx.textAlign = "center";
     this.ctx.fillText("Click to start", this.width/2, this.height-20);
     this.posCache = Array(8);
+    this.audioWritePosition = 0;
+    this.audioTailPosition = 0;
+    this.audioTail = null;
+    this.audio = new Audio();
+    this.audio.mozSetup(1, this.audioSampleRate);
   },
   difficulty: function H_difficulty() {
     return Math.max(100, 4*this.height/5-this.offset/200-65);
@@ -142,13 +198,9 @@ Helicopter.prototype = {
   },
   startGame: function H_startGame() {
     this.init();
-    if (this.settings.sound)
-      this.audio.play();
     this.main();
   },
   stopGame: function H_stopGame() {
-    this.audio.pause();
-    this.rev.pause();
     this.runId = 0;
 
     if (this.highscore < this.offset/10) {
@@ -228,7 +280,6 @@ Helicopter.prototype = {
       this.runId = window.requestAnimationFrame(this.drawExplosion.bind(this));
     } else {
       this.runId = null;
-      //window.requestAnimationFrame(this.drawCourse.bind(this));
     }
   },
   main: function H_main() {
@@ -237,6 +288,9 @@ Helicopter.prototype = {
       this.fps = Math.floor(1000/(now-this.lastDraw));
       this.lastDraw = now;
     }
+
+    if(this.settings.sound)
+      this.handleAudio();
     this.drawCourse();
 
     // draw the player
@@ -297,5 +351,5 @@ Helicopter.prototype = {
   }
 }
 window.addEventListener("load", function D_onload() {
-  new Helicopter(document.getElementById("game"), {sound: false, fps: false, width: 480, height: 320 });
+  new Helicopter(document.getElementById("game"), {sound: true, fps: false, width: 480, height: 320 });
 }, false);
